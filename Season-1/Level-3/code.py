924922ee9c9315fb1eec35a68458af24be1202ce
Yshnav29 <vyshnavard@gmail.com>
2024-06-16 20:10:20 +0530

    Bug fixed in Level-3

--- Season-1/Level-3/code.py
+++ Season-1/Level-3/code.py
@@ -13,6 +13,12 @@ def source():
     TaxPayer('foo', 'bar').get_prof_picture(request.args["input"])
 ### Unrelated to the exercise -- Ends here -- Please ignore
 
+def safe_path(path):
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        filepath = os.path.normpath(os.path.join(base_dir, path))
+        if base_dir != os.path.commonpath([base_dir, filepath]):
+            return None
+        return filepath
 class TaxPayer:
 
     def __init__(self, username, password):
@@ -23,33 +29,29 @@ class TaxPayer:
 
     # returns the path of an optional profile picture that users can set
     def get_prof_picture(self, path=None):
-        # setting a profile picture is optional
         if not path:
             pass
 
-        # defends against path traversal attacks
-        if path.startswith('/') or path.startswith('..'):
+        safe_path_result = safe_path(path)
+        if safe_path_result is None:
             return None
 
-        # builds path
-        base_dir = os.path.dirname(os.path.abspath(__file__))
-        prof_picture_path = os.path.normpath(os.path.join(base_dir, path))
-
-        with open(prof_picture_path, 'rb') as pic:
+        with open(safe_path_result, 'rb') as pic:
             picture = bytearray(pic.read())
 
-        # assume that image is returned on screen after this
-        return prof_picture_path
+        return safe_path_result
+
 
     # returns the path of an attached tax form that every user should submit
     def get_tax_form_attachment(self, path=None):
-        tax_data = None
-
         if not path:
             raise Exception("Error: Tax form is required for all users")
 
-        with open(path, 'rb') as form:
+        safe_path_result = safe_path(path)
+        if safe_path_result is None:
+            return None
+
+        with open(safe_path_result, 'rb') as form:
             tax_data = bytearray(form.read())
 
-        # assume that tax data is returned on screen after this
-        return path
+        return safe_path_result
